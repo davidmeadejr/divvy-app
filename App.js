64861b84feef6214939af8ae935147df6132ca2c
David Meade Jr <davidmeadejnr@gmail.com>
2022-09-27 13:11:42 +0100

    Project structure refactored.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>Hello World!</Text>
+      <Text style={styles.titleText}>Hello World!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
