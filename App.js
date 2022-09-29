990c6a339ee67ca59015af11848913da512d18f9
Zac <zac@zacmoss.co.uk>
2022-09-29 12:26:45 +0100

    pushing for allen

--- App.js
+++ App.js
@@ -1,8 +1,84 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import React, { useState } from "react";
+import Realm from "realm";
 
-export default function App() {
+const TaskSchema = {
+  name: "Task",
+  properties: {
+    _id: "int",
+    name: "string",
+    status: "string?",
+  },
+  primaryKey: "_id",
+};
+
+// const CustomerSchema = {
+//   name: "Customer",
+//   properties: {
+//     name: "String"
+//   }
+// }
+
+(async () => {
+  // use realm to interact with database
+  const realm = await Realm.open({
+    path: "myrealm",
+    schema: [TaskSchema],
+  });
+
+  // ### write records to database
+  // realm.write(() => {
+  //   task1 = realm.create("Task", {
+  //     _id: 1,
+  //     name: "go grocery shopping",
+  //     dateCreated: Date.now(),
+  //     status: "Open",
+  //   });
+  // });
+  //   task2 = realm.create("Task", {
+  //     _id: 2,
+  //     name: "go exercise",
+  //     dateCreated: Date.now(),
+  //     status: "Open",
+  //   });
+  //   console.log(`created two tasks: ${task1.name} & ${task2.name}`);
+  // });
+
+  // ### read records from database
+  const tasks = realm.objects("Task");
+  console.log(
+    `The lists of tasks are: ${tasks.map((task) => {
+      return task.name + " " + task._id + "\n\r";
+    })}`
+  );
+  console.log(tasks[0]);
+
+  // ### read ONE record from database
+  // const myTask = realm.objectForPrimaryKey("Task", 1637096347792); // search for a realm object with a primary key that is an int.
+  // console.log(`got task: ${myTask.name} & ${myTask._id}`);
+
+  // ### modify ONE record from database
+  // realm.write(() => {
+  //   let myTask = realm.objectForPrimaryKey("Task", 1637096347792);
+  //   console.log(`original task: ${myTask.name} & ${myTask._id} ${myTask.status}`);
+  //   myTask.status = "Closed"
+  // });
+
+  // ### delete ONE record from database
+  // realm.write(() => {
+  //   try {
+  //     let myTask = realm.objectForPrimaryKey("Task", 1637096312440);
+  //     realm.delete(myTask);
+  //     console.log("deleted task ");
+  //     myTask = null;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // });
+})();
+
+const App = () => {
   const [countOranges, setCountOranges] = useState(0);
   const [countApples, setCountApples] = useState(0);
   const [priceOfOranges, setPriceOfOranges] = useState(0);
@@ -10,6 +86,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Text></Text>
       <Text style={styles.orangeEmoji}>🍊</Text>
       <Text style={styles.titleText}>£0.30p each{"\n"}</Text>
       <Text style={styles.appleEmoji}>🍎</Text>
@@ -61,7 +138,7 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -93,3 +170,5 @@ const styles = StyleSheet.create({
     fontSize: 60,
   },
 });
+
+export default App;
