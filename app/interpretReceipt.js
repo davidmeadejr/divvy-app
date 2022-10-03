f7001894957197cc3e19a10bde98acc90770404f
Zac <zac@zacmoss.co.uk>
2022-10-03 16:22:05 +0100

    refactored argument tests

--- app/interpretReceipt.js
+++ app/interpretReceipt.js
@@ -2,9 +2,11 @@ export default interpretReceipt = (responseObj) => {
   if (!responseObj) return [];
   console.log(typeof responseObj);
   if (
-    typeof responseObj === "string" ||
-    Array.isArray(responseObj) ||
-    Number.isInteger(responseObj)
+    !(
+      typeof responseObj === "object" &&
+      responseObj !== null &&
+      !Array.isArray(responseObj)
+    )
   )
     throw new Error("Incorrect datatype in argument for interpretReceipt");
 };
