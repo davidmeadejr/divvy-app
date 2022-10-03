74cd56952027e69909b448e55e5a7609a9bf6a6e
Zac <zac@zacmoss.co.uk>
2022-10-03 22:36:07 +0100

    further refactoring

--- app/interpretReceipt.js
+++ app/interpretReceipt.js
@@ -1,8 +1,10 @@
 export default interpretReceipt = (responseObj) => {
   if (!responseObj) return [];
   verifyArgumentIsObject(responseObj);
-  if (responseObj.amounts.length > 1) {
+
+  if (!parseFloat(responseObj.amounts[0].text[0])) {
     return responseObj.amounts.map((item) => {
+      if (responseObj.totalAmount.data === item.data) return;
       name = item.text;
       if (name.includes(responseObj.amounts[0].data.toString())) {
         const nameAsArray = name.split(" ");
@@ -15,7 +17,6 @@ export default interpretReceipt = (responseObj) => {
 
   let name = responseObj.amounts[0].text;
   let quantity = 1;
-  if (responseObj.totalAmount.data === responseObj.amounts[0].data) return [];
   if (parseFloat(name[0])) {
     quantity = parseFloat(name[0]);
     const nameAsArray = name.split(" ");
@@ -23,11 +24,6 @@ export default interpretReceipt = (responseObj) => {
     name = nameAsArray.join(" ");
   }
 
-  if (name.includes(responseObj.amounts[0].data.toString())) {
-    const nameAsArray = name.split(" ");
-    nameAsArray.splice(nameAsArray.length - 1, 1);
-    name = nameAsArray.join(" ");
-  }
   return Array(quantity).fill({
     amount: responseObj.amounts[0].data,
     name: name,
