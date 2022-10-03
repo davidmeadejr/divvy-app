90a599672ccb87c13a934aae47377c0b22a07417
Zac <zac@zacmoss.co.uk>
2022-10-03 23:27:36 +0100

    refactored

--- app/interpretReceipt.js
+++ app/interpretReceipt.js
@@ -1,6 +1,35 @@
 export default interpretReceipt = (responseObj) => {
   if (!responseObj) return [];
   verifyArgumentIsObject(responseObj);
+  const filteredArray = filterArrayForInvalidEntries(responseObj);
+
+  if (!parseFloat(filteredArray[0].text[0])) {
+    return filteredArray.map((item) => {
+      if (responseObj.totalAmount.data === item.data) return;
+      let itemName = item.text;
+      if (itemName.includes(filteredArray[0].data.toString())) {
+        const nameAsArray = itemName.split(" ");
+        nameAsArray.splice(nameAsArray.length - 1, 1);
+        itemName = nameAsArray.join(" ");
+      }
+      return { amount: item.data, name: itemName };
+    });
+  }
+  return duplicateEntriesForItemQuantity(filteredArray);
+};
+
+const verifyArgumentIsObject = (responseObj) => {
+  if (
+    !(
+      typeof responseObj === "object" &&
+      responseObj !== null &&
+      !Array.isArray(responseObj)
+    )
+  )
+    throw new Error("Incorrect datatype in argument for interpretReceipt");
+};
+
+const filterArrayForInvalidEntries = (responseObj) => {
   const filteredArray = [];
   const filteredArrayText = [];
   responseObj.amounts.forEach((item) => {
@@ -13,42 +42,19 @@ export default interpretReceipt = (responseObj) => {
       filteredArrayText.push(item.text);
     }
   });
+  return filteredArray;
+};
 
-  if (!parseFloat(filteredArray[0].text[0])) {
-    return filteredArray.map((item) => {
-      if (responseObj.totalAmount.data === item.data) return;
-      name = item.text;
-      if (name.includes(filteredArray[0].data.toString())) {
-        const nameAsArray = name.split(" ");
-        nameAsArray.splice(nameAsArray.length - 1, 1);
-        name = nameAsArray.join(" ");
-      }
-      return { amount: item.data, name: name };
-    });
-  }
-
+const duplicateEntriesForItemQuantity = (filteredArray) => {
   let name = filteredArray[0].text;
   let quantity = 1;
-  if (parseFloat(name[0])) {
-    quantity = parseFloat(name[0]);
-    const nameAsArray = name.split(" ");
-    nameAsArray.splice(0, 1);
-    name = nameAsArray.join(" ");
-  }
+  quantity = parseFloat(name[0]);
+  const nameAsArray = name.split(" ");
+  nameAsArray.splice(0, 1);
+  name = nameAsArray.join(" ");
 
   return Array(quantity).fill({
     amount: filteredArray[0].data,
     name: name,
   });
-};
-
-const verifyArgumentIsObject = (responseObj) => {
-  if (
-    !(
-      typeof responseObj === "object" &&
-      responseObj !== null &&
-      !Array.isArray(responseObj)
-    )
-  )
-    throw new Error("Incorrect datatype in argument for interpretReceipt");
 };
