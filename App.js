247a2e6382d2429678a640d9dedce7a213879265
David Meade Jr <davidmeadejnr@gmail.com>
2022-10-05 01:50:08 +0100

    Fix: Files App.js and styles.js refactored.

--- App.js
+++ App.js
@@ -1,140 +1,60 @@
-import { StatusBar } from "expo-status-bar";
-// Imports that allow you to use "react" and "react-natives" features.
-import { Pressable, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import { RealmProvider } from "./app/createRealmContext";
-// import MealScreen from "./app/components/mealScreen";
-import SelectMealSplash from "./app/components/selectMealSplash";
-
-import UploadReceipt from "./app/components/uploadReceipt";
 import { NavigationContainer, useFocusEffect } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, Image, ImageBackground, Button, TouchableHighlight } from "react-native";
-import addItem from "./app/components/addItem";
-
+import { View, Text, Image, ImageBackground, TouchableHighlight } from "react-native";
 import { launchCamera, launchImageLibrary } from "react-native-image-picker";
-import uploadReceipt from "./app/components/uploadReceipt";
-import AddItem from "./app/components/addItem";
 import Items from "./app/components/items";
 import { useRealm } from "./app/createRealmContext";
 import { Meal } from "./app/models/Meal";
 import styles from "./app/common/styles";
 import Users from "./app/components/users";
 
+/*
+ * Home Screen Functionality
+ */
 const HomeScreen = ({ navigation }) => {
   return (
     <ImageBackground
+      style={styles.homeScreenBackground}
       source={require("./assets/background-image.png")}
       resizeMode={"cover"}
-      style={{ flex: 1, width: "100%", height: "100%" }}
     >
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Image
-          source={require("./assets/adaptive-icon.png")}
-          style={{ width: 350, height: 350, backgroundColor: "rgb(75,35,243,0.3)", marginTop: -320 }}
-        />
-        <TouchableHighlight style={{ marginBottom: 32 }} onPress={() => navigation.navigate("Camera Screen")}>
-          <Text
-            style={{
-              fontSize: 32,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              padding: 10,
-              overflow: "hidden",
-              fontWeight: "bold",
-            }}
-          >
-            New Meals 🍽️
-          </Text>
+      <View style={styles.homeScreenContainer}>
+        <Image style={styles.divvyLogo} source={require("./assets/adaptive-icon.png")} />
+        <TouchableHighlight style={styles.newMealsButton} onPress={() => navigation.navigate("Camera Screen")}>
+          <Text style={styles.newMealsButtonText}>New Meals 🍽️</Text>
         </TouchableHighlight>
         <TouchableHighlight>
-          <Text
-            style={{
-              fontSize: 32,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              paddingTop: 10,
-              paddingBottom: 10,
-              overflow: "hidden",
-              fontWeight: "bold",
-              paddingLeft: 20,
-              paddingRight: 20,
-            }}
-          >
-            My Meals 🍲
-          </Text>
+          <Text style={styles.myMealsButtonText}>My Meals 🍲</Text>
         </TouchableHighlight>
       </View>
     </ImageBackground>
   );
 };
 
+/*
+ * Camera Screen Functionality
+ */
 const CameraScreen = ({ navigation, selectedMeal, setSelectedMeal, testVar }) => {
-  // const [selectedMeal, setSelectedMeal] = useState();
   const [createNewMeal, setCreateNewMeal] = useState(false);
-
   const [imageSource, setImageSource] = useState();
   const [imageObj, setImageObj] = useState();
   const realm = useRealm();
 
   return (
-    // <>
     <ImageBackground
       source={require("./assets/background-image-two.png")}
       resizeMode={"cover"}
-      style={{ flex: 1, width: "100%", height: "100%" }}
+      style={styles.cameraScreenBackground}
     >
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "space-evenly",
-        }}
-      >
+      <View style={styles.cameraScreenContainer}>
         <TouchableHighlight onPress={() => navigation.navigate("🏠")}>
-          <Text
-            style={{
-              fontSize: 32,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              paddingTop: 10,
-              paddingBottom: 10,
-              overflow: "hidden",
-              fontWeight: "bold",
-              paddingLeft: 20,
-              paddingRight: 20,
-              marginBottom: 30,
-            }}
-          >
-            ⬅ Back
-          </Text>
+          <Text style={styles.cameraScreenBackButton}>⬅ Back</Text>
         </TouchableHighlight>
         <TouchableHighlight>
           <Text
-            style={{
-              fontSize: 32,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              paddingTop: 10,
-              paddingBottom: 10,
-              overflow: "hidden",
-              fontWeight: "bold",
-              paddingLeft: 20,
-              paddingRight: 20,
-              marginBottom: 30,
-            }}
+            style={styles.createButton}
             onPress={() => {
               let newMeal;
               realm.write(() => {
@@ -180,109 +100,40 @@ const CameraScreen = ({ navigation, selectedMeal, setSelectedMeal, testVar }) =>
             )
           }
         >
-          <Text
-            style={{
-              fontSize: 32,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              paddingTop: 10,
-              paddingBottom: 10,
-              overflow: "hidden",
-              fontWeight: "bold",
-              paddingLeft: 20,
-              paddingRight: 20,
-              marginBottom: 30,
-            }}
-          >
-            Upload 📁
-          </Text>
+          <Text style={styles.uploadButton}>Upload 📁</Text>
         </TouchableHighlight>
-        {/* <View style={{ flex: 0, alignItems: "center", padding: 16, backgroundColor: "#333" }}> */}
         <TouchableHighlight onPress={() => navigation.navigate("Save Photo Screen")}>
-          <Text
-            style={{
-              fontSize: 32,
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              paddingTop: 10,
-              paddingBottom: 10,
-              overflow: "hidden",
-              fontWeight: "bold",
-              paddingLeft: 20,
-              paddingRight: 20,
-              marginBottom: 30,
-            }}
-          >
-            📸
-          </Text>
+          <Text style={styles.cameraEmojiButton}>📸</Text>
         </TouchableHighlight>
-        {/* </View> */}
-        {/* </> */}
       </View>
     </ImageBackground>
   );
 };
 
+/*
+ * Save Photo Screen Functionality
+ */
 const SavePhotoScreen = ({ navigation }) => {
   return (
     <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "space-between",
-          backgroundColor: "#333",
-          flexDirection: "row",
-          paddingLeft: 16,
-          paddingRight: 16,
-        }}
-      >
+      <View style={styles.savePhotoContainer}>
         <TouchableHighlight onPress={() => navigation.navigate("Camera Screen")}>
-          <Text
-            style={{
-              fontSize: 20,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              padding: 8,
-              overflow: "hidden",
-              marginTop: 40,
-            }}
-          >
-            ⬅ Retake
-          </Text>
+          <Text style={styles.retakePhotoButton}>⬅ Retake</Text>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => navigation.navigate("Camera Screen")}>
-          <Text
-            style={{
-              fontSize: 20,
-              color: "#fff",
-              borderWidth: 2,
-              borderColor: "#4B23F3",
-              borderRadius: 10,
-              backgroundColor: "#4B23F3",
-              padding: 8,
-              overflow: "hidden",
-              marginTop: 40,
-            }}
-          >
-            Use
-          </Text>
+          <Text style={styles.usePhotoButton}>Use</Text>
         </TouchableHighlight>
       </View>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#333" }}>
-        <Text style={{ color: "#fff" }}>"Itemised Receipt functionality"</Text>
+      <View style={styles.photoScreenshotContainer}>
+        <Text style={styles.photoScreenshot}>"Itemised Receipt functionality"</Text>
       </View>
     </>
   );
 };
 
+/*
+ * Meal Screen Functionality
+ */
 const MealScreen = ({ navigation, route }) => {
   const [selectedMeal, setSelectedMeal] = useState(route.params.selectedMeal);
   const [selectedFriend, setSelectedFriend] = useState();
@@ -295,14 +146,15 @@ const MealScreen = ({ navigation, route }) => {
         selectedFriend={selectedFriend}
         setSelectedFriend={setSelectedFriend}
       />
-      {/* <Users /> */}
     </View>
   );
 };
 
 const Stack = createNativeStackNavigator();
 
-// The main component which acts as a container for all other components within the the codebase.
+/*
+ * The main component which acts as a container for all other components within the the codebase.
+ */
 const App = () => {
   return (
     <NavigationContainer>
@@ -311,7 +163,6 @@ const App = () => {
         <Stack.Screen name="Camera Screen" component={CameraScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Save Photo Screen" component={SavePhotoScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Meal Screen" component={MealScreen} options={{ headerShown: false }} />
-        {/* <Stack.Screen name="Upload Photo" component={UploadPhoto} options={{ headerShown: false }} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/common/styles.js
+++ app/common/styles.js
@@ -81,4 +81,191 @@ export default styles = StyleSheet.create({
     marginBottom: 16,
     marginLeft: 16,
   },
+
+  /* *************************************************** */
+
+  /*
+   * App.js styles
+   */
+
+  /* *************************************************** */
+
+  /*
+   * HomeScreen
+   */
+  homeScreenBackground: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+
+  homeScreenContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  divvyLogo: {
+    width: 350,
+    height: 350,
+    backgroundColor: "rgb(75,35,243,0.3)",
+    marginTop: -320,
+  },
+
+  newMealsButton: {
+    marginBottom: 32,
+  },
+
+  newMealsButtonText: {
+    fontSize: 32,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    padding: 10,
+    overflow: "hidden",
+    fontWeight: "bold",
+  },
+
+  myMealsButtonText: {
+    fontSize: 32,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    paddingTop: 10,
+    paddingBottom: 10,
+    overflow: "hidden",
+    fontWeight: "bold",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  /*
+   * CameraScreen
+   */
+
+  cameraScreenBackground: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+
+  cameraScreenContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+
+  cameraScreenBackButton: {
+    fontSize: 32,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    paddingTop: 10,
+    paddingBottom: 10,
+    overflow: "hidden",
+    fontWeight: "bold",
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 30,
+  },
+
+  createButton: {
+    fontSize: 32,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    paddingTop: 10,
+    paddingBottom: 10,
+    overflow: "hidden",
+    fontWeight: "bold",
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 30,
+  },
+
+  uploadButton: {
+    fontSize: 32,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    paddingTop: 10,
+    paddingBottom: 10,
+    overflow: "hidden",
+    fontWeight: "bold",
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 30,
+  },
+
+  cameraEmojiButton: {
+    fontSize: 32,
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    paddingTop: 10,
+    paddingBottom: 10,
+    overflow: "hidden",
+    fontWeight: "bold",
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 30,
+  },
+
+  /*
+   * Save Photo Screen
+   */
+  savePhotoContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    backgroundColor: "#333",
+    flexDirection: "row",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+
+  retakePhotoButton: {
+    fontSize: 20,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    padding: 8,
+    overflow: "hidden",
+    marginTop: 40,
+  },
+
+  usePhotoButton: {
+    fontSize: 20,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: "#4B23F3",
+    borderRadius: 10,
+    backgroundColor: "#4B23F3",
+    padding: 8,
+    overflow: "hidden",
+    marginTop: 40,
+  },
+
+  photoScreenshotContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#333",
+  },
+
+  photoScreenshot: {
+    color: "#fff",
+  },
 });
