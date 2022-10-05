6f618f8765d4f347047a2effe9eed0434edea103
Zac <zac@zacmoss.co.uk>
2022-10-05 23:39:59 +0100

    changed pressable to touchable opacity

--- app/components/itemComponent.js
+++ app/components/itemComponent.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { useRealm } from "../createRealmContext";
-import { Text, View, Pressable, Alert, Image } from "react-native";
+import {
+  Text,
+  View,
+  Pressable,
+  Alert,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import styles from "../common/styles";
 
-export default ItemComponent = ({ selectedFriend, item, selectedMeal, setSelectedMeal }) => {
+export default ItemComponent = ({
+  selectedFriend,
+  item,
+  selectedMeal,
+  setSelectedMeal,
+}) => {
   const realm = useRealm();
   const itemFriends = (item) => {
     if (!item.friends.length) return [];
@@ -19,9 +31,13 @@ export default ItemComponent = ({ selectedFriend, item, selectedMeal, setSelecte
 
   const itemOnPressAddFriend = (item) => {
     if (selectedFriend) {
-      const friendIdx = item.friends.map((friend) => friend._id.toString()).indexOf(selectedFriend._id.toString());
+      const friendIdx = item.friends
+        .map((friend) => friend._id.toString())
+        .indexOf(selectedFriend._id.toString());
       realm.write(() => {
-        friendIdx === -1 ? item.friends.push(selectedFriend) : item.friends.splice(friendIdx, 1);
+        friendIdx === -1
+          ? item.friends.push(selectedFriend)
+          : item.friends.splice(friendIdx, 1);
       });
       setSelectedMeal(realm.objectForPrimaryKey("Meal", selectedMeal._id));
     }
@@ -29,17 +45,22 @@ export default ItemComponent = ({ selectedFriend, item, selectedMeal, setSelecte
 
   return (
     <View>
-      <Pressable
+      <TouchableOpacity
         style={styles.itemContainer}
         onPress={() => itemOnPressAddFriend(item)}
         // onLongPress={() => handleLongPress(item)}
       >
         <View style={styles.itemNameContainer}>
-          <Text style={styles.redCancel} onLongPress={() => handleLongPress(item)}>
+          <Text
+            style={styles.redCancel}
+            // onLongPress={() => handleLongPress(item)}
+          >
             ❌
           </Text>
-          <Text style={styles.itemName} onLongPress={() => handleLongPress(item)}>
-            {/* <Image style={styles.redCancel} source={require("../../assets/red-x.png")} /> */}
+          <Text
+            style={styles.itemName}
+            // onLongPress={() => handleLongPress(item)}
+          >
             {item.name}
           </Text>
         </View>
@@ -47,7 +68,7 @@ export default ItemComponent = ({ selectedFriend, item, selectedMeal, setSelecte
           <Text style={styles.amount}>£{item.amount.toFixed(2)}</Text>
           <Text style={styles.friend}>{itemFriends(item)}</Text>
         </View>
-      </Pressable>
+      </TouchableOpacity>
     </View>
   );
 };
