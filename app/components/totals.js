895351958b1379d2cd0a3872b4d56c10a2ff12b3
Zac <zac@zacmoss.co.uk>
2022-10-07 11:02:31 +0100

    fixed rounding bug

--- app/components/totals.js
+++ app/components/totals.js
@@ -1,55 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, FlatList } from "react-native";
 import styles from "../common/styles";
 
 export default Totals = ({ selectedMeal }) => {
   const getChargeText = (charge) => {
-    const chargeName =
-      charge === "Service charge" ? "serviceCharge" : charge.toLowerCase();
+    const chargeName = charge;
     if (selectedMeal[`${chargeName}Amount`]) {
       let chargeAmount = selectedMeal[`${chargeName}Amount`];
       if (selectedMeal[`${chargeName}Type`] === "percent")
         chargeAmount = getPercentageForString(
           selectedMeal[`${chargeName}Amount`]
         );
-      return (
-        <Text
-          style={{ fontSize: 16, color: "#fff" }}
-        >{`${charge}: £${chargeAmount.toFixed(2)}`}</Text>
-      );
+      return chargeAmount;
     }
   };
 
   const getPercentageForString = (charge) => (getSubTotal() * charge) / 100;
 
-  const getFriendTotalsText = (item) => {
-    return (
-      <View>
-        <View style={styles.totalsBreakdownContainer}>
-          <Text style={styles.totalsItemName}>{item.name}</Text>
-          <Text style={styles.totalsItemAmount}>
-            £{getIndividualTotal(item).toFixed(2)}
-          </Text>
-        </View>
-        <Text
-          style={{
-            marginBottom: 20,
-            color: "white",
-          }}
-        >
-          {getIndividualItems(item)}
-        </Text>
-      </View>
-    );
-  };
-
   const getIndividualTotal = (friend) => {
     const itemsTotal = friend.items
-      .map((item) => roundToTwo(item.amount / item.friends.length))
+      .map((item) => item.amount / item.friends.length)
       .reduce((a, b) => a + b, 0);
-    return roundToTwo(
-      roundToTwo(itemsTotal) + getAddedChargesSummed(itemsTotal, true)
-    );
+    return itemsTotal + getAddedChargesSummed(itemsTotal, true);
   };
 
   const getAddedChargesSummed = (subtotal, forIndividualFriend) =>
@@ -57,13 +29,10 @@ export default Totals = ({ selectedMeal }) => {
       .map((charge) => {
         let chargeResult;
         if (selectedMeal[`${charge}Type`] === "percent") {
-          chargeResult = roundToTwo(
-            (subtotal * selectedMeal[`${charge}Amount`]) / 100
-          );
+          chargeResult = (subtotal * selectedMeal[`${charge}Amount`]) / 100;
         } else if (forIndividualFriend) {
-          chargeResult = roundToTwo(
-            selectedMeal[`${charge}Amount`] / selectedMeal.friends.length
-          );
+          chargeResult =
+            selectedMeal[`${charge}Amount`] / selectedMeal.friends.length;
         } else {
           chargeResult = selectedMeal[`${charge}Amount`];
         }
@@ -94,49 +63,102 @@ export default Totals = ({ selectedMeal }) => {
     );
   };
 
+  const returnAllTotals = () => {
+    const amounts = {
+      subTotal: getSubTotal(),
+      total: getTotal(getSubTotal()),
+    };
+    ["serviceCharge", "tip", "tax", "discount"].forEach((charge) => {
+      amounts[charge] = roundToTwo(getChargeText(charge));
+    });
+    selectedMeal.friends.forEach(
+      (friend) =>
+        (amounts[friend._id.toString()] = roundToTwo(
+          getIndividualTotal(friend)
+        ))
+    );
+    if (!!selectedMeal.friends.length) {
+      const friendsTotal = selectedMeal.friends
+        .map((friend) => amounts[friend._id.toString()])
+        .reduce((a, b) => a + b, 0);
+      if (friendsTotal < amounts.total)
+        amounts[selectedMeal.friends[0]._id.toString()] += 0.01;
+    }
+
+    const charges = ["serviceCharge", "tip", "tax", "discount"];
+    return (
+      <View>
+        <View style={styles.totalsInfoContainer}>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              color: "#fff",
+            }}
+          >
+            <Text style={styles.subTotalsInfoTitle}>
+              Subtotal: £{amounts.subTotal.toFixed(2)}
+            </Text>
+            <Text style={styles.totalsInfoTitle}>
+              Total: £{amounts.total.toFixed(2)}
+            </Text>
+          </View>
+          <FlatList
+            data={["Service charge", "Tip", "Tax", "Discount"]}
+            renderItem={({ item, index }) => {
+              const chargeText = charges[index];
+              if (amounts[chargeText])
+                return (
+                  <Text
+                    style={{ fontSize: 16, color: "#fff" }}
+                  >{`${item}: £${amounts[chargeText].toFixed(2)}`}</Text>
+                );
+            }}
+            keyExtractor={(charge) => charge}
+          />
+        </View>
+        <FlatList
+          style={{ height: getFlatListHeight() }}
+          data={selectedMeal.friends}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                <View style={styles.totalsBreakdownContainer}>
+                  <Text style={styles.totalsItemName}>{item.name}</Text>
+                  <Text style={styles.totalsItemAmount}>
+                    £{amounts[item._id.toString()].toFixed(2)}
+                  </Text>
+                </View>
+                <Text
+                  style={{
+                    marginBottom: 20,
+                    color: "white",
+                  }}
+                >
+                  {getIndividualItems(item)}
+                </Text>
+              </View>
+            );
+          }}
+          keyExtractor={(friend) => friend._id.toString()}
+        />
+      </View>
+    );
+  };
+
   const getFlatListHeight = () => {
-    const heights = ["54%", "51%", "48%", "45%", "42%"];
+    console.log(selectedMeal);
+    const heights = ["69%", "66%", "62%", "59%", "55%"];
     const heightsIdx = [
       selectedMeal.serviceChargeAmount,
       selectedMeal.tipAmount,
       selectedMeal.taxAmount,
       selectedMeal.discountAmount,
     ].filter(Boolean).length;
+    console.log(heightsIdx);
     return heights[heightsIdx];
   };
 
-  return (
-    <View>
-      <View style={styles.totalsInfoContainer}>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            color: "#fff",
-          }}
-        >
-          <Text style={styles.subTotalsInfoTitle}>
-            Subtotal: £{getSubTotal().toFixed(2)}
-          </Text>
-          <Text style={styles.totalsInfoTitle}>
-            Total: £{getTotal(getSubTotal()).toFixed(2)}
-          </Text>
-        </View>
-        <FlatList
-          data={["Service charge", "Tip", "Tax", "Discount"]}
-          renderItem={({ item }) => getChargeText(item)}
-          keyExtractor={(charge) => charge}
-        />
-      </View>
-      {/* <View style={styles.totalsBreakdownContainer}> */}
-      <FlatList
-        style={{ height: getFlatListHeight() }}
-        data={selectedMeal.friends}
-        renderItem={({ item }) => getFriendTotalsText(item)}
-        keyExtractor={(friend) => friend._id.toString()}
-      />
-      {/* </View> */}
-    </View>
-  );
+  return <View>{returnAllTotals()}</View>;
 };
